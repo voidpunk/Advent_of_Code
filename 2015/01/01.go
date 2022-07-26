87882ad346b91b18b99ce6182d8e75becf321020
Nil ☿ <voidpunk.glitch@gmail.com>
2022-07-26 04:49:18 +0200

    2015/01 - Go v 1.1

--- 2015/01/01.go
+++ 2015/01/01.go
@@ -7,10 +7,14 @@ import (
 )
 
 func main() {
+
 	raw_data, _ := os.ReadFile("./input.txt")
 	problem_input := string(raw_data)
+
 	open_parentheses := strings.Count(problem_input, "(")
 	closed_parentheses := strings.Count(problem_input, ")")
 	difference := open_parentheses - closed_parentheses
+	
 	fmt.Println(difference)
+
 }
