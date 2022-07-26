8f6efa974ee451f03f58b980e193712e0991bf25
Nil ☿ <voidpunk.glitch@gmail.com>
2022-07-26 13:00:07 +0200

    2015/01 - JavaScript v 1.1

--- 2015/01/01.js
+++ 2015/01/01.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 
 const problem_input = fs.readFileSync('./input.txt', 'utf8');
+
 const open_parentheses = problem_input.match(/\(/g).length;
 const closed_parentheses = problem_input.match(/\)/g).length;
 const difference = open_parentheses - closed_parentheses;
