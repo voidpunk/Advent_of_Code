d5154bd553afbe7f10cc9a163f18a035d9bd41fd
Nil ☿ <voidpunk.glitch@gmail.com>
2022-07-26 22:29:52 +0200

    2015/02 - Go v 1.0

---
+++ 2015/02/02.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+)
+
+func main() {
+
+	raw_data, _ := os.ReadFile("./input.txt")
+	problem_input := strings.Split(string(raw_data), "\n")
+
+	total := 0
+	for _, e := range problem_input {
+		inputs := strings.Split(e, "x")
+		total += calculateWrappingPaper(inputs)
+	}
+
+	fmt.Println(total)
+
+}
+
+func calculateWrappingPaper(inputs []string) int {
+	x, _ := strconv.Atoi(inputs[0])
+	y, _ := strconv.Atoi(inputs[1])
+	z, _ := strconv.Atoi(inputs[2])
+	surfaces := []int{x*y, y*z, z*x}
+	return 2*sum(surfaces) + min(surfaces)
+}
+
+func sum(arr []int) int {
+	res := 0
+	for _, e := range arr {
+		res += e
+	}
+	return res
+}
+
+func min(arr []int) int {
+	res := arr[0]
+	for _, e := range arr {
+		if e < res {
+			res = e
+		}
+	}
+	return res
+}
