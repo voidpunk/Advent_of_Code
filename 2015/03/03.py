25a6a0ff35187d694e132bd784498e4c62445261
Nil ☿ <voidpunk.glitch@gmail.com>
2022-08-06 01:36:58 +0200

    2015/03 - Python v 1.3

--- 2015/03/03.py
+++ 2015/03/03.py
@@ -1,6 +1,9 @@
 with open('./input.txt', 'r') as file:
     problem_input = file.read()
 
+# ------------------------------------------------------------------------------
+# Pt. 1
+
 coordinates = [0, 0]
 coordinates_list = [tuple(coordinates)]
 
@@ -16,6 +19,30 @@ for direction in problem_input:
         case '^':
             coordinates[1] += 1
     
+    coordinates_list.append(tuple(coordinates))
+
+print(len(set(coordinates_list)))
+
+# ------------------------------------------------------------------------------
+# Pt. 2
+
+coord_santa, coord_robot = [0, 0], [0, 0]
+coordinates_list = [tuple(coord_santa), tuple(coord_robot)]
+
+for sorter, direction in enumerate(problem_input, 1):
+
+    coordinates = coord_santa if sorter % 2 != 0 else coord_robot
+
+    match direction:
+        case '<':
+            coordinates[0] -= 1
+        case '>':
+            coordinates[0] += 1
+        case 'v':
+            coordinates[1] -= 1
+        case '^':
+            coordinates[1] += 1
+
     coordinates_list.append(tuple(coordinates))
 
 print(len(set(coordinates_list)))
