56bd12deccb87681d43aed9a3c3c4041439248c9
Nil ☿ <voidpunk.glitch@gmail.com>
2022-08-06 01:58:58 +0200

    2015/04 - Python v 1.1

--- 2015/04/04.py
+++ 2015/04/04.py
@@ -7,13 +7,11 @@ for i in range(10_000_000):
     string_key = problem_input + str(i)
     result = hashlib.md5(string_key.encode()).hexdigest()
 
-    # --------------------------------------------------------------------------
     # Pt. 1
     if run_once and result[:5] == '00000':
         run_once = False
         print(i)
 
-    # --------------------------------------------------------------------------
     # Pt. 2
     if result[:6] == '000000':
         print(i)
