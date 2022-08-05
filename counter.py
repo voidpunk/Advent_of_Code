296bcb9f875f111e237b96c2c1b5e16e95ef647e
Nil ☿ <voidpunk.glitch@gmail.com>
2022-08-05 16:01:59 +0200

    improvements to plotting func & updated plots

--- counter.py
+++ counter.py
@@ -1,17 +1,24 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 import os
 
 lang_dict = {
     'nim': 'Nim',
     'go' : 'Go',
-    'js' : 'JS',
+    'js' : 'Node.js',
     'rs' : 'Rust',
     'py' : 'Python',
     'cpp': 'C++',
+    'c'  : 'C',
     'jl' : 'Julia',
     'txt': 'Text'
 }
+
+def func(pct, data):
+    val = int(np.round(pct/100.*np.sum(list(data.values()))))
+    key = list(data.keys())[list(data.values()).index(val)]
+    return "{:.1f} %\n({})".format(pct, data[key])
 
 for year in os.listdir():
     if os.path.isdir(year) and year[:2] == '20':
@@ -25,13 +32,18 @@ for year in os.listdir():
                 with open(path, 'r') as file:
                     length = len(file.read())
                 len_dict[name] = length
-            print(len_dict)
             del len_dict['Text']
+            len_dict = {k: v for k, v in sorted(
+                len_dict.items(), key=lambda item: item[1], reverse=True)
+                }
+            print(len_dict)
             plt.figure(figsize=(6,6))
             plt.pie(
                 x=len_dict.values(),
                 labels=len_dict.keys(),
-                colors=sns.color_palette('muted')
+                colors=sns.color_palette('colorblind'),
+                autopct=lambda pct: func(pct, len_dict),
+                pctdistance=0.75
                 )
             plt.savefig(os.path.join(year, day, 'plot.png'), format='png')
             print()
