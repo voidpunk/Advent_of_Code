ef3465ae6d02f19a273ddfbb7d49c9d72b58e8d5
Nil ☿ <voidpunk.glitch@gmail.com>
2023-01-04 14:15:27 +0100

    2015/01 - Rust v 1.1

--- 2015/01/01.rs
+++ 2015/01/01.rs
@@ -1,9 +1,6 @@
-use std::fs;
-
 fn main() {
 
-    let contents = fs::read_to_string("./input.txt")
-        .expect("Something went wrong reading the file");
+    let contents = std::fs::read_to_string("./input.txt").unwrap();
 
     let open_parentheses = contents.matches("(").count();
     let closed_parentheses = contents.matches(")").count();
@@ -11,3 +8,5 @@ fn main() {
 
     println!("{}", difference);
 }
+
+// 0.3 ms ± 0.4 ms (10k runs, 1k warmup) - Rustc 1.66.0: -opt-level=3
